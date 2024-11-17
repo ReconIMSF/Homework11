f8ac74df67643aa7fa9d3596e0fb74df681a9ce6
ReconIMSF <dorbatorg@gmail.com>
2024-11-17 11:28:34 +0800

    Homework 11 is corrected

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.time.Instant;
 import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -20,20 +19,23 @@ public class Main {
 
     // Задача 2 - программа, которая определяет текущий год, год выпуска устройства и сообщает, какую версию установить.
     // Проверка года выпуска, версии ПО и распечатка - в методе
-    public static void programVersion (int currentYear, short yearOfProduction, byte clientOS) {
-        boolean androLight = yearOfProduction < currentYear && clientOS == 1;
-        boolean iOSLight = yearOfProduction < currentYear && clientOS == 0;
-        boolean andro = yearOfProduction == currentYear && clientOS == 1;
-        boolean iOS = yearOfProduction == currentYear && clientOS == 0;
-        if (androLight) {
+    public static void programVersion (int currentYear, int yearOfProduction, byte clientOS) {
+        boolean androLight = yearOfProduction < 2015 && clientOS == 1;
+        boolean iOSLight = yearOfProduction < 2015 && clientOS == 0;
+        boolean androOld = yearOfProduction >= 2015 && yearOfProduction < currentYear && clientOS == 1;
+        boolean iOSOld = yearOfProduction >= 2015 && yearOfProduction < currentYear && clientOS == 0;
+        boolean andro = yearOfProduction >= 2015 && yearOfProduction == currentYear && clientOS == 1;
+        boolean iOS = yearOfProduction >= 2015 && yearOfProduction == currentYear && clientOS == 0;
+        if (androLight || androOld) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (iOSLight) {
+        } else if (iOSLight || iOSOld) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (andro) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if (iOS) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+        System.out.println(currentYear);
     }
 
     // Задача 3 - программа, которая определяет срок доставки карты.
@@ -59,7 +61,7 @@ public class Main {
         short year = 2021;
         leapYear(year);
         System.out.println("Задача 2");
-        short yearOfProductuion = 2015;
+        int yearOfProductuion = 2015;
         int currentYear = LocalDate.now().getYear();
         byte clientOS = 1;
         programVersion(currentYear, yearOfProductuion, clientOS);
@@ -67,42 +69,4 @@ public class Main {
         short deliveryDistance = 95;
         deliveryTime(deliveryDistance);
     }
-//    Задача 4
-//        System.out.println("Задача 4");
-//    short deliveryDistance = 95;
-//    int deliveryTime = 0;
-//        if (deliveryDistance <= 20) {
-//        deliveryTime += 1;
-//        System.out.println("Потребуется дней: " + deliveryTime);
-//    } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-//        deliveryTime += 2;
-//        System.out.println("Потребуется дней: " + deliveryTime);
-//    } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-//        deliveryTime += 3;
-//        System.out.println("Потребуется дней: " + deliveryTime);
-//    } else {
-//        System.out.println("Cвыше 100 км доставки нет");
-//    }
-
-//
-//
-//        Задача 3
-//        Возвращаемся к задаче на расчет дней доставки банковской карты.
-//                Текст прошлого задания
-//        В банке для клиентов организовывается доставка карт на дом.Чтобы известить клиента о том, когда будет доставлена
-//        его карта, нужно знать расстояние от офиса до адреса доставки.
-//                Правила доставки такие:
-//        Доставка в пределах 20 км занимает сутки.
-//                Доставка в пределах от 20 км до 60 км добавляет еще один день доставки.
-//        Доставка в пределах от 60 км до 100 км добавляет еще одни сутки.
-//                Свыше 100 км доставки нет.
-//                То есть с каждым следующим интервалом доставки срок увеличивается на 1 день.
-//                Напишите программу, которая выдает сообщение в консоль: «Потребуется дней: ... »+срок доставки.
-//                Объявите целочисленную переменную
-//        deliveryDistance = 95, которая содержит дистанцию до клиента.
-//
-//        Ваша задача — доработать код, а именно написать метод, который на вход принимает дистанцию и возвращает
-//        итоговое количество дней доставки.
-
-
 }
